9818786e7adbfa1a66bea92d8f296f22e20d2180
Gocario <Gocario.dev@gmail.com>
2015-11-26 22:35:46 +0100

    Initial Commit

---
+++ include/keystate.h
@@ -0,0 +1,24 @@
+#ifndef KEYSTATE_H
+#define KEYSTATE_H
+
+#include <3ds/types.h>
+#include <3ds/services/hid.h>
+
+typedef struct
+{
+	u32 down;
+	u32 held;
+	u32 up;
+	touchPosition touch;
+} keystate_s;
+
+inline void hidKeys(keystate_s* ks)
+{
+	hidScanInput();
+	ks->down = hidKeysDown();
+	ks->held = hidKeysHeld();
+	ks->up = hidKeysUp();
+	hidTouchRead(&ks->touch);
+}
+
+#endif // KEYSTATE_H
---
+++ include/types.h
@@ -0,0 +1,20 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+#define U64_MAX	UINT64_MAX
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
+
+#endif // TYPES_H
